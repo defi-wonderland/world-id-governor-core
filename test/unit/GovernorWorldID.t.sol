2a76fcf855d6451c460a360f513a0ba0602af137
0xAustrian <austrian@defi.sucks>
2024-03-01 16:11:48 -0300

    test: add test case for castVote

--- test/unit/GovernorWorldID.t.sol
+++ test/unit/GovernorWorldID.t.sol
@@ -12,13 +12,26 @@ import {IVotes} from 'open-zeppelin/governance/utils/IVotes.sol';
 abstract contract Base is Test {
   IERC20 public token;
   IGovernorWorldID public governor;
+  IWorldID public worldID;
+  uint256 public proposalId;
 
   function setUp() public virtual {
     // Deploy token
     token = new MockERC20Votes();
 
+    // Deploy mock worldID
+    worldID = IWorldID(makeAddr('worldID'));
+    vm.etch(address(worldID), new bytes(0x1));
+
     // Deploy governor
-    governor = new MockGovernorWorldId(IWorldID(address(this)), 'appId', 'actionId', IVotes(address(token)));
+    governor = new MockGovernorWorldId(worldID, 'appId', 'actionId', IVotes(address(token)));
+
+    // Create proposal
+    proposalId = governor.propose(new address[](1), new uint256[](1), new bytes[](1), '0x');
+
+    // Advance time assuming 1 block == 1 second (this will make the proposal active)
+    vm.warp(block.timestamp + governor.votingDelay() + 1);
+    vm.roll(block.number + governor.votingDelay() + 1);
   }
 }
 
@@ -34,8 +47,35 @@ contract UnitGovernorWorldIDCastVoteDisabled is Base {
   }
 
   // TODO: fix this
-  function testcastVoteBySigWithReasonShouldBeDisabled() public {
+  function testCastVoteBySigWithReasonShouldBeDisabled() public {
     // vm.expectRevert(IGovernorWorldID.GovernorWorldID_NotSupportedFunction.selector);
     // governor.castVoteBySig(0, 0, address(0), "");
   }
+}
+
+contract UnitGovernorWorldIDCastVoteWithParams is Base {
+  function testCastVoteWithReasonAndParamsShouldWork(
+    uint256 _root,
+    uint256 _nullifierHash,
+    uint256[8] memory _proof
+  ) public {
+    // Encode the parameters
+    bytes memory _params = abi.encode(_root, _nullifierHash, _proof);
+
+    // Mock
+    vm.mockCall(address(worldID), abi.encodeWithSelector(IWorldID.verifyProof.selector), abi.encode(0));
+
+    // Cast the vote
+    governor.castVoteWithReasonAndParams(proposalId, 0, '', _params);
+  }
+
+  function testCastVoteWithReasonAndParamsBySigShouldWork() public {}
+
+  function testCastVoteWithReasonAndParamsShouldRevertIfNullifierAlreadyUsed() public {}
+
+  function testCastVoteWithReasonAndParamsBySigShouldRevertIfNullifierAlreadyUsed() public {}
+
+  function testCastVoteWithReasonAndParamsShouldRevertIfProofIsNotValid() public {}
+
+  function testCastVoteWithReasonAndParamsBySigShouldRevertIfProofIsNotValid() public {}
 }
