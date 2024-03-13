d097e78679ea10ff2c2b795037671f839733becc
Disco <131301107+0xDiscotech@users.noreply.github.com>
2024-03-13 15:51:36 +0100

    style: clean and order (#7)

--- test/unit/GovernorDemocratic.t.sol
+++ test/unit/GovernorDemocratic.t.sol
@@ -12,22 +12,17 @@ import {IVotes} from 'open-zeppelin/governance/utils/IVotes.sol';
 import {IERC20} from 'open-zeppelin/token/ERC20/IERC20.sol';
 
 abstract contract Base is Test, UnitUtils {
-  uint8 public constant SUPPORT = 1;
   uint256 public constant GROUP_ID = 1;
-  string public constant REASON = '';
-  uint256 public constant WEIGHT = 1;
+  uint256 public constant ONE = 1;
 
   IERC20 public token;
   IGovernorWorldID public governor;
   IWorldIDRouter public worldIDRouter;
   IWorldID public worldID;
 
-  uint256 public proposalId;
   address public user;
 
-  function setUp() public virtual {
-    vm.clearMockedCalls();
-
+  function setUp() public {
     user = makeAddr('user');
 
     // Deploy token
@@ -52,14 +47,6 @@ abstract contract Base is Test, UnitUtils {
     string memory _appId = 'appId';
     string memory _actionId = 'actionId';
     governor = new MockGovernorDemocratic(GROUP_ID, worldIDRouter, _appId, _actionId, IVotes(address(token)));
-
-    // Create proposal
-    string memory _description = '0xDescription';
-    proposalId = governor.propose(new address[](1), new uint256[](1), new bytes[](1), _description);
-
-    // Advance time assuming 1 block == 1 second (this will make the proposal active)
-    vm.warp(block.timestamp + governor.votingDelay() + 1);
-    vm.roll(block.number + governor.votingDelay() + 1);
   }
 }
 
@@ -68,7 +55,8 @@ contract GovernorDemocratic_Unit_GetVotes is Base {
    * @notice Check that the voting weight is 1
    */
   function test_returnsOne(address _account, uint256 _timepoint, bytes memory _params) public {
-    uint256 _votes = IMockGovernorDemocraticForTest(address(governor)).forTest_getVotes(_account, _timepoint, _params);
-    assertEq(_votes, 1);
+    uint256 _votingWeight =
+      IMockGovernorDemocraticForTest(address(governor)).forTest_getVotes(_account, _timepoint, _params);
+    assertEq(_votingWeight, ONE);
   }
 }
