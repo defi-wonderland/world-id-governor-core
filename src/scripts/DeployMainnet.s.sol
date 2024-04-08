4e7d3b82df7b59ce49c05ee02408ccfe3bcc942a
0xAustrian <austrian@defi.sucks>
2024-04-08 13:22:40 -0300

    feat: add new deploy scripts, tweak parameters in DeploySepolia

--- src/scripts/DeployMainnet.s.sol
+++ src/scripts/DeployMainnet.s.sol
@@ -5,17 +5,17 @@ import {DemocraticGovernance} from 'contracts/DemocraticGovernance.sol';
 import {Script, console} from 'forge-std/Script.sol';
 import {IWorldIDRouter} from 'interfaces/IWorldIDRouter.sol';
 
-contract DeploySepolia is Script {
+contract DeployMainnet is Script {
   uint256 public constant GROUP_ID = 1;
-  IWorldIDRouter public constant WORLD_ID_ROUTER = IWorldIDRouter(0x469449f251692E0779667583026b5A1E99512157);
+  IWorldIDRouter public constant WORLD_ID_ROUTER = IWorldIDRouter(0x163b09b4fE21177c455D850BD815B6D583732432);
   uint256 public constant QUORUM = 5;
   uint48 public constant INITIAL_VOTING_DELAY = 0;
   uint32 public constant INITIAL_VOTING_PERIOD = 3 days;
   uint256 public constant INITIAL_PROPOSAL_THRESHOLD = 0;
-  uint256 public constant ROOT_EXPIRATION_THRESHOLD = 1 hours;
+  uint256 public constant ROOT_EXPIRATION_THRESHOLD = 0;
 
-  string public appId = vm.envString('SEPOLIA_APP_ID');
-  address public deployer = vm.rememberKey(vm.envUint('SEPOLIA_DEPLOYER_PK'));
+  string public appId = vm.envString('MAINNET_APP_ID');
+  address public deployer = vm.rememberKey(vm.envUint('MAINNET_DEPLOYER_PK'));
 
   function run() public {
     vm.startBroadcast(deployer);
--- src/scripts/DeployOptimism.s.sol
+++ src/scripts/DeployOptimism.s.sol
@@ -5,17 +5,17 @@ import {DemocraticGovernance} from 'contracts/DemocraticGovernance.sol';
 import {Script, console} from 'forge-std/Script.sol';
 import {IWorldIDRouter} from 'interfaces/IWorldIDRouter.sol';
 
-contract DeploySepolia is Script {
+contract DeployOptimism is Script {
   uint256 public constant GROUP_ID = 1;
-  IWorldIDRouter public constant WORLD_ID_ROUTER = IWorldIDRouter(0x469449f251692E0779667583026b5A1E99512157);
+  IWorldIDRouter public constant WORLD_ID_ROUTER = IWorldIDRouter(0x57f928158C3EE7CDad1e4D8642503c4D0201f611);
   uint256 public constant QUORUM = 5;
   uint48 public constant INITIAL_VOTING_DELAY = 0;
   uint32 public constant INITIAL_VOTING_PERIOD = 3 days;
   uint256 public constant INITIAL_PROPOSAL_THRESHOLD = 0;
   uint256 public constant ROOT_EXPIRATION_THRESHOLD = 1 hours;
 
-  string public appId = vm.envString('SEPOLIA_APP_ID');
-  address public deployer = vm.rememberKey(vm.envUint('SEPOLIA_DEPLOYER_PK'));
+  string public appId = vm.envString('OPTIMISM_APP_ID');
+  address public deployer = vm.rememberKey(vm.envUint('OPTIMISM_DEPLOYER_PK'));
 
   function run() public {
     vm.startBroadcast(deployer);
