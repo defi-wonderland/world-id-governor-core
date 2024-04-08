fb3d42a65888fee0b5dc8f0d8f2f1f895b129d1a
0xAustrian <austrian@defi.sucks>
2024-04-08 18:51:52 -0300

    feat: add public to constants

--- src/scripts/Constants.sol
+++ src/scripts/Constants.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.8.23;
 
 abstract contract Constants {
-  uint256 constant GROUP_ID = 1;
-  uint256 constant QUORUM = 5;
-  uint48 constant INITIAL_VOTING_DELAY = 1;
-  uint32 constant INITIAL_VOTING_PERIOD = 3 days;
-  uint256 constant INITIAL_PROPOSAL_THRESHOLD = 0;
+  uint256 public constant GROUP_ID = 1;
+  uint256 public constant QUORUM = 5;
+  uint48 public constant INITIAL_VOTING_DELAY = 1;
+  uint32 public constant INITIAL_VOTING_PERIOD = 3 days;
+  uint256 public constant INITIAL_PROPOSAL_THRESHOLD = 0;
 }
