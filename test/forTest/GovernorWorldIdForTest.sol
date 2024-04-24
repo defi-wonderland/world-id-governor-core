8b420f8545aa7c99dbabc5f3f7f36412fd8b288d
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-04-24 12:54:35 -0300

    refactor: update return arg on for test quorum

--- test/forTest/GovernorWorldIdForTest.sol
+++ test/forTest/GovernorWorldIdForTest.sol
@@ -73,8 +73,8 @@ contract GovernorWorldIdForTest is InternalCallsWatcherExtension, GovernorCounti
     _proposalId = _propose(_targets, _values, _calldatas, _description, _proposer);
   }
 
-  function quorum(uint256) public view override returns (uint256 _quorum) {
-    _quorum = block.number;
+  function quorum(uint256) public pure override returns (uint256 _randomQuorum) {
+    _randomQuorum = 10;
   }
 
   function clock() public view override returns (uint48) {
