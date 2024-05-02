058e55888142aafb36602ecb87e8199c13ecc9c8
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-05-02 19:26:29 -0300

    chore: update root exp threshold on deployment files

--- src/scripts/DeployOpSepolia.s.sol
+++ src/scripts/DeployOpSepolia.s.sol
@@ -6,7 +6,7 @@ import {Deploy} from 'scripts/Deploy.sol';
 
 contract DeployOpSepolia is Deploy {
   IWorldIDRouter public constant WORLD_ID_ROUTER = IWorldIDRouter(0x11cA3127182f7583EfC416a8771BD4d11Fae4334);
-  uint256 public constant ROOT_EXPIRATION_THRESHOLD = 1 hours;
+  uint256 public constant ROOT_EXPIRATION_THRESHOLD = 2 hours;
 
   string public appId = vm.envString('OP_SEPOLIA_APP_ID');
   address public deployer = vm.rememberKey(vm.envUint('OP_SEPOLIA_DEPLOYER_PK'));
