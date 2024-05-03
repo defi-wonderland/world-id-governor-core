7a8c666c2f98154ec6c4db60f5864636d90a356b
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-05-03 12:12:42 -0300

    chore: update scripts config

--- src/scripts/DeployOptimism.s.sol
+++ src/scripts/DeployOptimism.s.sol
@@ -6,7 +6,7 @@ import {Deploy} from 'scripts/Deploy.sol';
 
 contract DeployOptimism is Deploy {
   IWorldIDRouter public constant WORLD_ID_ROUTER = IWorldIDRouter(0x57f928158C3EE7CDad1e4D8642503c4D0201f611);
-  uint256 public constant ROOT_EXPIRATION_THRESHOLD = 1 hours;
+  uint256 public constant ROOT_EXPIRATION_THRESHOLD = 2 hours;
 
   string public appId = vm.envString('OPTIMISM_APP_ID');
   address public deployer = vm.rememberKey(vm.envUint('OPTIMISM_DEPLOYER_PK'));
